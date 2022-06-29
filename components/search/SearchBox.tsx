1e9d3af795f18215662da96ab522a411525ec1f3
suman <ronaldosuman97@gmail.com>
2022-06-29 09:23:22 +0530

    search function

--- components/search/SearchBox.tsx
+++ components/search/SearchBox.tsx
@@ -4,20 +4,46 @@ import { AiOutlineSearch } from "react-icons/ai";
 import { client } from "../../pages/_app";
 import { GET_20_MOVIES } from "../../graphql/queries";
 import { ClipLoader } from "react-spinners";
+import { useLazyQuery } from "@apollo/client";
+
+interface Movie {
+  movie_id: number;
+  movie_image: string;
+  movie_rating: string;
+  movie_release: number;
+  movie_name: string;
+  movie_type: string;
+}
+
 export function SearchBox() {
   const searchRef = useRef<HTMLInputElement>(null);
   const [loading, setLoading] = useState(false);
+  const [getMovies] = useLazyQuery(GET_20_MOVIES);
 
   function onSearchEnter(e: KeyboardEvent<HTMLInputElement>) {
-    const searchTex = searchRef.current?.value;
+    const searchText = searchRef.current?.value;
     const movieCache = client.readQuery({
       query: GET_20_MOVIES,
     });
 
-    if (searchTex && e.key == "Enter") {
+    if (searchText && e.key == "Enter") {
       setLoading(true);
       if (movieCache) {
-        console.log(searchTex, e.key);
+        const searchResult = movieCache.randommovies!.filter((m: Movie) => {
+          return m.movie_name.toLowerCase().includes(searchText.toLowerCase());
+        });
+        console.log(searchResult);
+        setLoading(false);
+      } else {
+        getMovies().then((res) => {
+          const searchResult = res.data!.randommovies.filter((m: Movie) => {
+            return m.movie_name
+              .toLowerCase()
+              .includes(searchText.toLowerCase());
+          });
+          console.log(searchResult);
+          setLoading(false);
+        });
       }
     }
   }
