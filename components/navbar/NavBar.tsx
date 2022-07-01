7fb48ff2a8eda953cf9297ae5d2a5e8d516dc3e6
suman <ronaldosuman97@gmail.com>
2022-07-01 12:41:09 +0530

    nav-small -a

--- components/navbar/NavBar.tsx
+++ components/navbar/NavBar.tsx
@@ -2,12 +2,39 @@ import { Logo } from "../logo/Logo";
 import styles from "./NavBar.module.scss";
 import { SearchBox } from "../search/SearchBox";
 import { NavLinks } from "../navlinks/NavLinks";
+import { useState } from "react";
+import Link from "next/link";
 
 export function NavBar() {
+  const [modal, setModal] = useState(false);
+  const handleModalClick = () => {
+    setModal((p) => !p);
+  };
+
   return (
     <nav className={styles.navbar}>
       <Logo />
       <NavLinks />
+      <div className={styles.nav_btn_container}>
+        <button onClick={handleModalClick} className={styles.nav_btn}>
+          <span className={styles.btn_line_1}></span>
+          <span className={styles.btn_line_2}></span>
+          <span className={styles.btn_line_3}></span>
+        </button>
+      </div>
+      {modal && (
+        <ul className={styles.nav_mobile_links}>
+          <Link href={"/movies"}>
+            <li>home</li>
+          </Link>
+          <Link href={"/movies"}>
+            <li>movies</li>
+          </Link>
+          <Link href={"/movies"}>
+            <li>new movies</li>
+          </Link>
+        </ul>
+      )}
       <SearchBox />
     </nav>
   );
