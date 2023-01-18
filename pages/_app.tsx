99bd7a8aa30c2c22b2d1b11508e31adb9349cefb
suman <ronaldosuman97@gmail.com>
2023-01-18 18:53:17 +0530

    chore: API endpoint updated

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import type { AppProps } from "next/app";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "https://e7zuxg9qv4.execute-api.ap-south-1.amazonaws.com/",
+  uri: process.env.NEXT_PUBLIC_API_URL,
   cache: new InMemoryCache(),
 });
 
